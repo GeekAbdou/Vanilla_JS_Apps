02c44625169caeb35ce972cb5d142fbc4d9cbac0
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2022-02-12 01:50:49 +0200

    (add edit done) functions completed

--- TODO App/main.js
+++ TODO App/main.js
@@ -3,26 +3,18 @@ const form = document.querySelector("#form");
 const todoInput = document.querySelector("#todoInput");
 const allTasks = document.querySelector("#allTasks");
 const filters = document.querySelectorAll(".nav-item");
+const alertDiv = document.querySelector("#message");
 
 let tasksList = [];
 
-//preview tasks
-const previewTasks = function (tasksList) {
-  allTasks.innerHTML = "";
-  if (tasksList.length > 0) {
-    tasksList.forEach((task) => {
-      let liTag = `<li class="list-group-item d-flex justify-content-between">
-                            <span>${task.name}</span>
-                            <span>
-                                <a href="#"><i class="bi bi-check2-circle text-success"></i></a>
-                                <a href="#"><i class="bi bi-pencil-square text-primary"></i></a>
-                                <a href="#"><i class="bi bi-x-circle text-danger"></i></a>
-                            </span>
-                        </li>`;
-      allTasks.insertAdjacentHTML("beforeend", liTag);
-    });
-  } else {
-  }
+//Alert message
+const alertMessage = function (message, className) {
+  alertDiv.innerHTML = message;
+  alertDiv.classList.add(className, "show", "transition");
+  setTimeout(() => {
+    alertDiv.classList.remove("show", "alert-danger", "alert-success");
+    alertDiv.innerHTML = "";
+  }, 2000);
 };
 
 //set Tasks in local
@@ -42,21 +34,106 @@ const getLocalStorage = function () {
   console.log(tasksList);
 };
 
+//preview tasks
+const previewTasks = function (tasksList) {
+  allTasks.innerHTML = "";
+
+  if (tasksList.length > 0) {
+    tasksList.forEach((task) => {
+      const iconClass = tasksList.isDone
+        ? "bi-check-circle-fill"
+        : "bi-check-circle";
+
+      let liTag = `<li class="list-group-item d-flex justify-content-between">
+                              <span class="title" data-time=${task.addedAt}>${task.name}</span>
+                              <span>
+                                  <a href="#" data-done><i class="bi ${iconClass} text-success"></i></a>
+                                  <a href="#" data-edit><i class="bi bi-pencil-square text-primary"></i></a>
+                                  <a href="#" data-delete><i class="bi bi-x-circle text-danger"></i></a>
+                              </span>
+                          </li>`;
+      allTasks.insertAdjacentHTML("beforeend", liTag);
+      handleItem(task);
+    });
+  } else {
+  }
+};
+
+//events on action buttons
+const handleItem = function (itemData) {
+  const items = document.querySelectorAll(".list-group-item");
+
+  items.forEach((item) => {
+    //done
+    if (
+      item.querySelector(".title").getAttribute("data-time") == itemData.addedAt //get the task by id(Date Added)
+    ) {
+      item.querySelector("[data-done]").addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const itemIndex = tasksList.indexOf(itemData);
+        const currentItem = tasksList[itemIndex];
+
+        const currentClass = currentItem.isDone
+          ? "bi-check-circle-fill"
+          : "bi-check-circle";
+
+        currentItem.isDone = currentItem.isDone ? false : true;
+        tasksList.splice(itemIndex, 1, currentItem);
+        setLocalStorage(tasksList);
+
+        const iconClass = currentItem.isDone
+          ? "bi-check-circle-fill"
+          : "bi-check-circle";
+
+        this.firstElementChild.classList.replace(currentClass, iconClass);
+        const filterType = document.querySelector("#tabValue").value;
+        //getItemsFilter(filterType);
+      });
+
+      //edit
+      item.querySelector("[data-edit]").addEventListener("click", function (e) {
+        e.preventDefault();
+        todoInput.value = itemData.name;
+        document.querySelector("#objIndex").value = tasksList.indexOf(itemData);
+      });
+    }
+  });
+};
+
+//update function
+const updateITask = function (currentItemIndex, value) {
+  const newItem = tasksList[currentItemIndex];
+  newItem.name = value;
+  tasksList.splice(currentItemIndex, 1, tasksList);
+  setLocalStorage(tasksList);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const newTask = todoInput.value.trim();
     if (newTask.length === 0) {
-      alert("Cannot be Empty!");
+      alertMessage("Cannot be Empty", "alert-danger");
     } else {
-      const newTodo = {
-        name: newTask,
-        isDone: false,
-      };
-      tasksList.push(newTodo);
-      setLocalStorage(tasksList);
+      const currentItemIndex = document.querySelector("#objIndex").value; //for editing tasks
+      if (currentItemIndex) {
+        //update
+        updateITask(currentItemIndex, newTask);
+        document.querySelector("#objIndex").value = "";
+        alertMessage("Task has been updated", "alert-success");
+      } else {
+        const newTodo = {
+          addedAt: new Date().getTime(), //works as is for the task
+          name: newTask,
+          isDone: false,
+        };
+        tasksList.push(newTodo);
+        setLocalStorage(tasksList);
+        alertMessage("New Task added Successfully", "alert-success");
+      }
     }
-    getLocalStorage()
+    getLocalStorage();
     console.log(tasksList);
   });
   getLocalStorage();
